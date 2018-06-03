95e4eabc4540fd818a055a5aef0352252fb963c2
Jaspreet Singh <jaspreet@joinhoney.com>
2018-06-03 14:23:15 -0700

    Add chap 18

--- EPI/Chap18/5/index.js
+++ EPI/Chap18/5/index.js
@@ -1,6 +1,7 @@
 const TwoSum = require('../SortedTwoSum');
 
 module.exports = (arr, target) => {
+  arr.sort();
   for (const num of arr) {
     if (TwoSum(arr, target - num)) {
       return true;
